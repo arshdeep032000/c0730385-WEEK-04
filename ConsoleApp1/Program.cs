b30628674ed22977b3ea887810e117d503131544
visualstudio <visualstudio@122-PC38>
2019-01-30 11:24:47 -0500

    i added a countryside class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,8 +16,35 @@ namespace ConsoleApp1
             // Node is an ADT
             // what KIND of DATA do we need in a NODE?
             public Village nextVillage;
-            public String VillageName;
+        public Village previousVillage;
+        public String VillageName;
         public bool isAstrildehere = false;
         }
+    class countryside
+    {
+        Village Maple = new Village();
+      
+        Village toronto = new Village();
+        
+            Village Ajax = new Village();
+        Village first;
+        Village last;
+        
+            public void Launch()
+        {
+            Maple.VillageName = "Maple";
+            Maple.nextVillage = toronto;
+            Maple.previousVillage = null;
+            toronto.VillageName = "toronto";
+            toronto.nextVillage = Ajax;
+            toronto.previousVillage = Maple;
+            Ajax.VillageName = "Ajax";
+            Ajax.nextVillage = null;
+            Ajax.nextVillage = toronto;
+
+
+
+        }
+    }
     
 }
